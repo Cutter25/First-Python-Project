a5595ca6376c0584d9bfbdfaed694e57a06a155e
Corbin Cutter <corbinc03123@gmail.com>
2023-09-11 11:14:59 -0400

    Forgot () after every .lower

--- quiz_game.py
+++ quiz_game.py
@@ -8,61 +8,61 @@ if start.lower != "yes":
 print("Awesome! Let's get started! :)")
 
 answer = input("What does JS stand for? ")
-if answer.lower == "javascript":
+if answer.lower() == "javascript":
     print("Correct!")
 else: 
     print("Incorrect!")
 
 answer = input("What does SQL stand for? ")
-if answer.lower == "structured query language":
+if answer.lower() == "structured query language":
     print("Correct!")
 else: 
     print("Incorrect!")
 
 answer = input("What does HTML stand for? ")
-if answer.lower == "hypertext markup language":
+if answer.lower() == "hypertext markup language":
     print("Correct!")
 else: 
     print("Incorrect!")
 
 answer = input("What is REACT? ")
-if answer.lower == "a javascript framework":
+if answer.lower() == "a javascript framework":
     print("Correct!")
 else: 
     print("Incorrect!")  
 
 answer = input("Who made REACT? ")
-if answer.lower == "facebook":
+if answer.lower() == "facebook":
     print("Correct!")
 else: 
     print("Incorrect!")
 
 answer = input("What is Python? ")
-if answer.lower == "a programming language":
+if answer.lower() == "a programming language":
     print("Correct!")
 else: 
     print("Incorrect!")  
 
 answer = input("What does CSS stand for? ")
-if answer.lower == "cascading syle sheets":
+if answer.lower() == "cascading syle sheets":
     print("Correct!")
 else: 
     print("Incorrect!")
 
 answer = input("What is NodeJS ? ")
-if answer.lower == "a javascript back-end runtime environment":
+if answer.lower() == "a javascript back-end runtime environment":
     print("Correct!")
 else: 
     print("Incorrect!")
 
 answer = input("In JavaScript, what does let declare? ")
-if answer.lower == "a variable that can be reassigned globally":
+if answer.lower() == "a variable that can be reassigned globally":
     print("Correct!")
 else: 
     print("Incorrect!")
 
 answer = input("In JavaScript, what does const declare? ")
-if answer.lower == "a variable that cannot be reassigned":
+if answer.lower() == "a variable that cannot be reassigned":
     print("Correct!")
 else: 
     print("Incorrect!")
