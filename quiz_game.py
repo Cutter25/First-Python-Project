975297ea5b6261d71113d66ca286ae19d7e678b7
Corbin Cutter <corbinc03123@gmail.com>
2023-09-11 11:03:58 -0400

    Questions complete. Funcitonality time

--- quiz_game.py
+++ quiz_game.py
@@ -10,6 +10,60 @@ print("Awesome! Let's get started! :)")
 answer = input("What does JS stand for? ")
 if answer == "JavaScript":
     print("Correct!")
+else: 
+    print("Incorrect!")
 
+answer = input("What does SQL stand for? ")
+if answer == "Structured Query Language":
+    print("Correct!")
+else: 
+    print("Incorrect!")
 
+answer = input("What does HTML stand for? ")
+if answer == "Hypertext Markup Language":
+    print("Correct!")
+else: 
+    print("Incorrect!")
+
+answer = input("What is REACT? ")
+if answer == "A JavaScript framework":
+    print("Correct!")
+else: 
+    print("Incorrect!")  
+
+answer = input("Who made REACT? ")
+if answer == "Facebook":
+    print("Correct!")
+else: 
+    print("Incorrect!")
+
+answer = input("What is Python? ")
+if answer == "A programming language":
+    print("Correct!")
+else: 
+    print("Incorrect!")  
+
+answer = input("What does CSS stand for? ")
+if answer == "Cascading Syle Sheets":
+    print("Correct!")
+else: 
+    print("Incorrect!")
+
+answer = input("What is NodeJS ? ")
+if answer == "A JavaScript back-end runtime environment":
+    print("Correct!")
+else: 
+    print("Incorrect!")
+
+answer = input("In JavaScript, what does let declare? ")
+if answer == "A variable that can be reassigned globally":
+    print("Correct!")
+else: 
+    print("Incorrect!")
+
+answer = input("In JavaScript, what does const declare? ")
+if answer == "A variable that cannot be reassigned":
+    print("Correct!")
+else: 
+    print("Incorrect!")
 
