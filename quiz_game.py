284699f4d194f3a292bdeb2a327788475f8943e5
Corbin Cutter <corbinc03123@gmail.com>
2023-09-11 10:48:48 -0400

    First question

---
+++ quiz_game.py
@@ -0,0 +1,15 @@
+print("Welcome to my Python coding quiz!")
+
+start = input("Do you want to play? ")
+
+if start != "yes":
+    quit()
+
+print("Awesome! Let's get started! :)")
+
+answer = input("What does JS stand for? ")
+if answer == "JavaScript":
+    print("Correct!")
+
+
+
